59d107ddf3fc0b3fcb8a244c4cf3e9526bbee6ed
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-21 10:44:17 +0300

    :bricks:Database:Atlas mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,18 @@ require("dotenv").config();
 var cors = require("cors");
 
 const port = process.env.PORT || 8000;
+const uri = process.env.ATLAS_URI;
+
+//database connection
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
