386af5a850eaf542cb9c90ae1b0efb63cd9053c6
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-21 12:26:21 +0300

    :sparkles:middleware:error middleware function

---
+++ backend/middleware/error.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  // Create a copy of the error object
+  let error = { ...err };
+  error.message = err.message;
+
+  // Handle CastError (Invalid MongoDB ObjectId)
+  if (err.name === "CastError") {
+    const message = `Resource not found with id: ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Handle Mongoose duplicate key violation error
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Handle Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => " " + val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Send a JSON response with the appropriate status code and error message
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server error",
+  });
+};
+
+module.exports = errorHandler;
