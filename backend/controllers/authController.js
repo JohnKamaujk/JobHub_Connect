69af149bf7775ccb2bb9ee81c22fe074af572717
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-23 12:05:59 +0300

    :sparkles:feat:User signup feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,23 @@
+const User = require("../models/userModel");
+const ErrorResponse = require("../utils/errorResponse");
+
+exports.signup = async (req, res, next) => {
+  const { email } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    return next(new ErrorResponse("User already exist", 400));
+  }
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.signin = (req, res) => {
   res.send("Hello from noddejs");
 };
