8c3f23ce6c2b1776edca199229b1ec8d415a62bb
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-24 16:06:32 +0300

    :sparkles:feat:User profile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { signup, signin, logout } = require("../controllers/authController");
+const { isAuthenticated, userProfile } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/signup", signup);
