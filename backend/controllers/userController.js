71785d6298551d929269c29e9ce86407b6181f64
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-25 12:09:09 +0300

    :sparkles:feat:user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require("../models/userModel");
+const ErrorResponse = require("../utils/errorResponse");
+
+exports.allUsers = async (req, res, next) => {
+  //enable pagination
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+  const count = await User.find({}).estimatedDocumentCount();
+
+  try {
+    const users = await User.find()
+      .sort({ createdAt: -1 })
+      .select("-password")
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+
+    res.status(200).json({
+      success: true,
+      users,
+      page,
+      pages: Math.ceil(count / pageSize),
+      count,
+    });
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
