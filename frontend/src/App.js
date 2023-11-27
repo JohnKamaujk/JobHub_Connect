a1514826257f6c0d0116386f1289eb07f04c7e5c
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-27 12:27:31 +0300

    :sparkles:feat:Application route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
-      <h1>JOb POrtal</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
