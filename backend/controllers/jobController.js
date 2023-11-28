5051ce56934137bd08df03aacce8bf9427c7a649
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-28 15:56:12 +0300

    :sparkles:feat:filter job by location

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -74,14 +74,33 @@ exports.showJobs = async (req, res, next) => {
   let cat = req.query.cat;
   let categ = cat !== "" ? cat : ids;
 
+  //jobs by location
+  let locations = [];
+  const jobByLocation = await Job.find({}, { location: 1 });
+  jobByLocation.forEach((val) => {
+    locations.push(val.location);
+  });
+  let setUniqueLocation = [...new Set(locations)];
+  let location = req.query.location;
+  let locationFilter = location !== "" ? location : setUniqueLocation;
+
   //enable pagination
   const pageSize = 5;
   const page = Number(req.query.pageNumber) || 1;
   //const count = await Job.find({}).estimatedDocumentCount();
-  const count = await Job.find({ ...keyword, jobType: categ }).countDocuments();
+  const count = await Job.find({
+    ...keyword,
+    jobType: categ,
+    location: locationFilter,
+  }).countDocuments();
 
   try {
-    const jobs = await Job.find({ ...keyword, jobType: categ })
+    const jobs = await Job.find({
+      ...keyword,
+      jobType: categ,
+      location: locationFilter,
+    })
+      .sort({ createdAt: -1 })
       .skip(pageSize * (page - 1))
       .limit(pageSize);
     res.status(200).json({
@@ -90,6 +109,7 @@ exports.showJobs = async (req, res, next) => {
       page,
       pages: Math.ceil(count / pageSize),
       count,
+      setUniqueLocation,
     });
   } catch (error) {
     next(error);
