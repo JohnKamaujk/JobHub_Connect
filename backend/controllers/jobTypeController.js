c0b6a63f40ecdc0d271388ad01841589d1268e84
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-28 10:59:22 +0300

    :sparkles:feat:create Job type controller

---
+++ backend/controllers/jobTypeController.js
@@ -0,0 +1,18 @@
+const JobType = require("../models/jobTypeModel");
+const ErrorResponse = require("../utils/errorResponse");
+
+//create job category
+exports.createJobType = async (req, res, next) => {
+  try {
+    const jobT = await JobType.create({
+      jobTypeName: req.body.jobTypeName,
+      user: req.user.id,
+    });
+    res.status(201).json({
+      success: true,
+      jobT,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
