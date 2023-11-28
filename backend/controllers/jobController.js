a5c3f10dfcfa592f031c1f8c7fbd8e6ba0e17959
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-28 15:38:11 +0300

    :sparkles:feat:filter job by title or category

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -51,3 +51,47 @@ exports.updateJob = async (req, res, next) => {
     next(error);
   }
 };
+
+//show jobs by category
+exports.showJobs = async (req, res, next) => {
+  //enable search
+  const keyword = req.query.keyword
+    ? {
+        title: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  // filter jobs by category ids
+  let ids = [];
+  const jobTypeCategory = await JobType.find({}, { _id: 1 });
+  jobTypeCategory.forEach((cat) => {
+    ids.push(cat._id);
+  });
+
+  let cat = req.query.cat;
+  let categ = cat !== "" ? cat : ids;
+
+  //enable pagination
+  const pageSize = 5;
+  const page = Number(req.query.pageNumber) || 1;
+  //const count = await Job.find({}).estimatedDocumentCount();
+  const count = await Job.find({ ...keyword, jobType: categ }).countDocuments();
+
+  try {
+    const jobs = await Job.find({ ...keyword, jobType: categ })
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+    res.status(200).json({
+      success: true,
+      jobs,
+      page,
+      pages: Math.ceil(count / pageSize),
+      count,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createJob, singleJob, updateJob } = require("../controllers/jobController");
+const {
+  createJob,
+  singleJob,
+  updateJob,
+  showJobs,
+} = require("../controllers/jobController");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
 //jobs routes
@@ -11,5 +16,7 @@ router.post("/job/create", isAuthenticated, isAdmin, createJob);
 router.get("/job/:id", singleJob);
 // /api/job/update/job_id
 router.put("/job/update/:job_id", isAuthenticated, isAdmin, updateJob);
+// /api/jobs/show
+router.get("/jobs/show", showJobs);
 
 module.exports = router;
