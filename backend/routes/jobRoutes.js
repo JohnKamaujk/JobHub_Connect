620beef67cad15835fd96e0ec4f01a5b2adb70d6
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-28 12:11:57 +0300

    :sparkles:feat:Job Route

---
+++ backend/routes/jobRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { createJob } = require("../controllers/jobController");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
+
+//jobs routes
+
+// /api/job/create
+router.post("/job/create", isAuthenticated, isAdmin, createJob);
