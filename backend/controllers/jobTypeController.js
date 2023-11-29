ba7f724b198e8e37d7c9d2c98f03f5e49ee0e957
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-29 11:06:17 +0300

    :sparkles:feat:filter job category controller

--- backend/controllers/jobTypeController.js
+++ backend/controllers/jobTypeController.js
@@ -29,3 +29,18 @@ exports.allJobTypes = async (req, res, next) => {
     next(error);
   }
 };
+
+//update job type
+exports.updateJobType = async (req, res, next) => {
+  try {
+    const jobT = await JobType.findByIdAndUpdate(req.params.type_id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      jobT,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/jobTypeRoutes.js
+++ backend/routes/jobTypeRoutes.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const {
   createJobType,
   allJobTypes,
+  updateJobType,
 } = require("../controllers/jobTypeController");
-const { isAuthenticated } = require("../middleware/auth");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
 //job type routes
 
 // /api/type/create
-router.post("/type/create", isAuthenticated, createJobType);
+router.post("/type/create", isAuthenticated, isAdmin, createJobType);
 // /api/type/jobs
 router.get("/type/jobs", allJobTypes);
+// /api/type/update/type_id
+router.put("/type/update/:type_id", isAuthenticated, isAdmin, updateJobType);
 
 module.exports = router;
