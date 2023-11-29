b621b47bf136d0085936ddbcbfe673a1ffbbb685
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-29 15:38:18 +0300

    :sparkles:feat:Dispatch job load action

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
-import { Box, Container, Stack } from "@mui/material";
+import {
+  Box,
+  Container,
+  Stack,
+  Card,
+  useTheme,
+  Typography,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
+import { jobLoadAction } from "../redux/actions/jobAction";
+import { useParams } from "react-router-dom";
 
 const Home = () => {
+  const { palette } = useTheme();
+  const dispatch = useDispatch();
+  const { keyword, location } = useParams();
+
+  const [page, setPage] = useState(1);
+  const [cat, setCat] = React.useState("");
+
+  useEffect(() => {
+    dispatch(jobLoadAction(page, keyword, cat, location));
+  }, [page, keyword, cat, location]);
+
   return (
     <>
       <Box sx={{ bgcolor: "#fafafa", minHeight: "100vh" }}>
@@ -14,7 +35,18 @@ const Home = () => {
             direction={{ xs: "column", sm: "row" }}
             spacing={{ xs: 1, sm: 2, md: 4 }}
           >
-            <Box sx={{ flex: 2, p: 2 }}></Box>
+            <Box sx={{ flex: 2, p: 2 }}>
+              <Card sx={{ minWidth: 150, mb: 3, mt: 3, p: 2 }}>
+                <Box sx={{ pb: 2 }}>
+                  <Typography
+                    component="h4"
+                    sx={{ color: palette.secondary.main, fontWeight: 600 }}
+                  >
+                    Filter job by category
+                  </Typography>
+                </Box>
+              </Card>
+            </Box>
             <Box sx={{ flex: 5, p: 2 }}></Box>
           </Stack>
         </Container>
