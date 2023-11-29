89ccaf433d384064fdde9d705dd17d70931088a0
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-29 15:02:26 +0300

    :lipstick:UI:improve UI with MUI

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import { Box, Container, Stack } from "@mui/material";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <Header />
-      <h1>Home page</h1>
+      <Box sx={{ bgcolor: "#fafafa", minHeight: "100vh" }}>
+        <Navbar />
+        <Header />
+        <Container>
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+          >
+            <Box sx={{ flex: 2, p: 2 }}></Box>
+            <Box sx={{ flex: 5, p: 2 }}></Box>
+          </Stack>
+        </Container>
+      </Box>
     </>
   );
 };
