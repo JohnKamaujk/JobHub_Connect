b529f0e9084bb5d7640c41ee8c3c978d52861419
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-30 12:00:02 +0300

    :sparkles:feat:combined reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { loadJobReducer } from "./reducers/jobReducer";
+import { loadJobTypeReducer } from "./reducers/jobTypeReducer";
 
 //combine reducers
 const reducer = combineReducers({
   loadJobs: loadJobReducer,
+  jobTypeAll: loadJobTypeReducer,
 });
 
 //initial state
