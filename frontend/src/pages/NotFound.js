e6cf56944bb8c13a0729aae5de812f151a4db99a
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-30 14:59:32 +0300

    :lipstick:UI:Not found page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,9 +1,23 @@
+import { Box } from "@mui/material";
 import React from "react";
+import Footer from "../component/Footer";
+import Navbar from "../component/Navbar";
 
 const NotFound = () => {
   return (
     <>
-      <h1>NotFound</h1>
+      <Navbar />
+      <Box
+        sx={{
+          height: "81vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Page not found!</h1>
+      </Box>
+      <Footer />
     </>
   );
 };
