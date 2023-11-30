cc6a4483c42aba36c3efb82859e50637edc6e4d9
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-30 14:56:52 +0300

    :sparkles:feat:Search jobs by keyword

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search/location/:location" element={<Home />} />
+            <Route path="/search/:keyword" element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
