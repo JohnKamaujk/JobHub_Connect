001b4fb7b5bdaf10fc9a953c2da29c4b10e1f4b1
JohnKamaujk <johnjkkamau@gmail.com>
2023-12-04 12:06:11 +0300

    :sparkles:feat:Job history component

--- frontend/src/pages/user/UserJobsHistory.js
+++ frontend/src/pages/user/UserJobsHistory.js
@@ -1,9 +1,37 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CardElement from "../../components/CardElement";
+import { userProfileAction } from "../../redux/actions/userAction";
 
 const UserJobsHistory = () => {
+  const { user } = useSelector((state) => state.userProfile);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(userProfileAction());
+  }, []);
+
   return (
     <>
-      <h1>UserJobsHistory</h1>
+      <Box>
+        <Typography variant="h4" sx={{ color: "#fafafa" }}>
+          Jobs History
+        </Typography>
+        <Box>
+          {user &&
+            user.jobsHistory.map((history, i) => (
+              <CardElement
+                key={i}
+                id={history._id}
+                jobTitle={history.title}
+                description={history.description}
+                category=""
+                location={history.location}
+              />
+            ))}
+        </Box>
+      </Box>
     </>
   );
 };
