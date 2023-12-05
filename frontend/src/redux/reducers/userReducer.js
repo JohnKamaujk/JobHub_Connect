c421238b11f89f71e2a617f74244133459b21716
JohnKamaujk <johnjkkamau@gmail.com>
2023-12-05 15:59:03 +0300

    :sparkles:feat:all users reducer

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -1,4 +1,8 @@
 import {
+  ALL_USER_LOAD_FAIL,
+  ALL_USER_LOAD_REQUEST,
+  ALL_USER_LOAD_RESET,
+  ALL_USER_LOAD_SUCCESS,
   USER_APPLY_JOB_FAIL,
   USER_APPLY_JOB_REQUEST,
   USER_APPLY_JOB_RESET,
@@ -92,6 +96,25 @@ export const userApplyJobReducer = (state = {}, action) => {
     case USER_APPLY_JOB_FAIL:
       return { loading: false, error: action.payload };
     case USER_APPLY_JOB_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+//all users reducer
+export const allUserReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case ALL_USER_LOAD_REQUEST:
+      return { loading: true, users: [] };
+    case ALL_USER_LOAD_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload.users,
+      };
+    case ALL_USER_LOAD_FAIL:
+      return { loading: false, users: [], error: action.payload };
+    case ALL_USER_LOAD_RESET:
       return {};
     default:
       return state;
