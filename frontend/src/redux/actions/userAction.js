dfc765926d13672922dc5263e499b60c5f5cd43d
JohnKamaujk <johnjkkamau@gmail.com>
2023-12-05 16:03:41 +0300

    :sparkles:feat:all users ctions

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import {
+  ALL_USER_LOAD_FAIL,
+  ALL_USER_LOAD_REQUEST,
+  ALL_USER_LOAD_SUCCESS,
   USER_APPLY_JOB_FAIL,
   USER_APPLY_JOB_REQUEST,
   USER_APPLY_JOB_SUCCESS,
@@ -88,5 +91,22 @@ export const userApplyJobAction = (job) => async (dispatch) => {
       payload: error.response.data.error,
     });
     toast.error(error.response.data.error);
+  }
+};
+
+//all user action
+export const allUserAction = () => async (dispatch) => {
+  dispatch({ type: ALL_USER_LOAD_REQUEST });
+  try {
+    const { data } = await axios.get("/api/allusers");
+    dispatch({
+      type: ALL_USER_LOAD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_USER_LOAD_FAIL,
+      payload: error.response.data.error,
+    });
   }
 };
