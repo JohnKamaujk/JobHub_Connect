04280bfcf100eada8aa7f374f1463590748f54ae
JohnKamaujk <johnjkkamau@gmail.com>
2023-12-05 12:32:19 +0300

    :sparkles:feat:SIngle job page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import Layout from "./pages/global/Layout";
 import UserJobsHistory from "./pages/user/UserJobsHistory";
 import UserInfoDashboard from "./pages/user/UserInfoDashboard";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import SingleJob from "./pages/SingleJob";
 
 //HOC
 const UserDashboardHOC = Layout(UserDashboard);
@@ -36,6 +37,7 @@ const App = () => {
               <Route path="/search/location/:location" element={<Home />} />
               <Route path="/search/:keyword" element={<Home />} />
               <Route path="/login" element={<LogIn />} />
+              <Route path="/job/:id" element={<SingleJob />} />
               <Route
                 path="/admin/dashboard"
                 element={
