d0bcea534da4eae97e3edb379c1be91474cffa14
JohnKamaujk <johnjkkamau@gmail.com>
2023-12-06 11:51:12 +0300

    :sparkles:User signup action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -16,6 +16,9 @@ import {
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
+  USER_SIGNUP_FAIL,
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
 } from "../constants/userConstant";
 
 export const userSignInAction = (user) => async (dispatch) => {
@@ -37,12 +40,32 @@ export const userSignInAction = (user) => async (dispatch) => {
   }
 };
 
+// user sign up action
+export const userSignUpAction = (user) => async (dispatch) => {
+  dispatch({ type: USER_SIGNUP_REQUEST });
+  try {
+    const { data } = await axios.post("/api/signup", user);
+
+    dispatch({
+      type: USER_SIGNUP_SUCCESS,
+      payload: data,
+    });
+    toast.success("Register Successfully!");
+  } catch (error) {
+    dispatch({
+      type: USER_SIGNUP_FAIL,
+      payload: error.response.data.error,
+    });
+    toast.error(error.response.data.error);
+  }
+};
+
 //log out action
 export const userLogoutAction = () => async (dispatch) => {
   dispatch({ type: USER_LOGOUT_REQUEST });
   try {
-    const { data } = await axios.get("/api/logout");
     localStorage.removeItem("userInfo");
+    const { data } = await axios.get("/api/logout");
     dispatch({
       type: USER_LOGOUT_SUCCESS,
       payload: data,
@@ -74,6 +97,23 @@ export const userProfileAction = () => async (dispatch) => {
   }
 };
 
+//all user action
+export const allUserAction = () => async (dispatch) => {
+  dispatch({ type: ALL_USER_LOAD_REQUEST });
+  try {
+    const { data } = await axios.get("/api/allusers");
+    dispatch({
+      type: ALL_USER_LOAD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_USER_LOAD_FAIL,
+      payload: error.response.data.error,
+    });
+  }
+};
+
 //user job apply action
 export const userApplyJobAction = (job) => async (dispatch) => {
   dispatch({ type: USER_APPLY_JOB_REQUEST });
@@ -91,22 +131,5 @@ export const userApplyJobAction = (job) => async (dispatch) => {
       payload: error.response.data.error,
     });
     toast.error(error.response.data.error);
-  }
-};
-
-//all user action
-export const allUserAction = () => async (dispatch) => {
-  dispatch({ type: ALL_USER_LOAD_REQUEST });
-  try {
-    const { data } = await axios.get("/api/allusers");
-    dispatch({
-      type: ALL_USER_LOAD_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ALL_USER_LOAD_FAIL,
-      payload: error.response.data.error,
-    });
   }
 };
